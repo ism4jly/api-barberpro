1a5fcd446000cc2c83232d353e66a3d68077267e
ism4jly <ismaelpaulofreitas@gmail.com>
2025-10-30 15:57:35 -0300

    feat(api): adicionar middleware global de tratamento de erros

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/teste", (req: Request, res: Response) => {
+  throw new Error("Erro de teste");
+
   return res.json({ ok: true });
 });
 
