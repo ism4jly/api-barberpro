e9637088d62ce036c507a448a77b497a54813716
ism4jly <ismaelpaulofreitas@gmail.com>
2025-10-31 11:36:15 -0300

    feat(api): adicionar estrutura de controllers e services

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import { Router, Request, Response } from "express";
 
+import { CreateUserController } from "./controllers/user/CreateUserController";
+
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  return res.json({ ok: true });
-});
+//router.get("/teste", (req: Request, res: Response) => {
+//  return res.json({ ok: true });
+//});
+
+// --- ROTAS USER ---
+const createUserController = new CreateUserController();
+router.post("/users", createUserController.handle);
 
 export { router };
