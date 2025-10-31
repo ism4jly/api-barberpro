97f22f4b274d2145a5619a1833776d3e5160d6f1
ism4jly <ismaelpaulofreitas@gmail.com>
2025-10-31 14:29:33 -0300

    feat(auth): implementar login de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
@@ -9,7 +10,7 @@ const router = Router();
 //});
 
 // --- ROTAS USER ---
-const createUserController = new CreateUserController();
-router.post("/users", createUserController.handle);
+router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
 
 export { router };
