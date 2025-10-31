8e90836e75a56c962c27d5d020f5ee646664c743
ism4jly <ismaelpaulofreitas@gmail.com>
2025-10-31 10:59:29 -0300

    feat(db): adicionar schema Prisma e migrations iniciais

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,6 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/teste", (req: Request, res: Response) => {
-  throw new Error("Erro de teste");
-
   return res.json({ ok: true });
 });
 
