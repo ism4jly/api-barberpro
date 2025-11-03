4224297ac3bc22e6b69b94f59223e540a9c75680
ism4jly <ismaelpaulofreitas@gmail.com>
2025-11-03 09:14:08 -0300

    feat:middleware para autenticação e verificar token JWT

---
+++ src/controllers/user/DetailUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { DetailUserService } from "../../services/user/DetailUserService";
+
+class DetailUserController {
+  async handle(request: Request, response: Response) {
+    const user_id = request.user_id;
+
+    console.log(user_id);
+
+    const userDetailService = new DetailUserService();
+
+    const detailUser = await userDetailService.execute();
+
+    return response.json(detailUser);
+  }
+}
+
+export { DetailUserController };
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,9 @@ import { Router, Request, Response } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+import isAuthenticated from "./middlewares/isAuthenticated";
 
 const router = Router();
 
@@ -12,5 +15,6 @@ const router = Router();
 // --- ROTAS USER ---
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
