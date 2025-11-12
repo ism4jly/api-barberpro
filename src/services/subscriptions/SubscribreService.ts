fe0000aba87e19faf96c0eef858edc1868c7ac72
ism4jly <ismaelpaulofreitas@gmail.com>
2025-11-12 09:55:45 -0300

    feat:criando rota de pagamento

--- src/services/subscriptions/SubscribreService.ts
+++ src/services/subscriptions/SubscribreService.ts
@@ -1,4 +1,5 @@
 import prismaClient from "../../prisma";
+import Stripe from "stripe";
 
 interface SubscribreRequest {
     user_id: string;
@@ -6,7 +7,59 @@ interface SubscribreRequest {
 
 class SubscribreService {
     async execute({ user_id }: SubscribreRequest) {
-        return user_id
+        const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+            apiVersion: "2025-10-29.clover",
+            appInfo: {
+                name: "barber",
+                version: "1",
+            },
+        });
+
+        const user = await prismaClient.user.findFirst({
+            where: {
+                id: user_id,
+            },
+        });
+
+        let customerId = user?.stripe_customer_id;
+
+        if (!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: user?.email,
+            });
+
+            await prismaClient.user.update({
+                where: {
+                    id: user_id,
+                },
+                data: {
+                    stripe_customer_id: stripeCustomer.id,
+                },
+            });
+
+            customerId = stripeCustomer.id;
+        }
+
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer: customerId,
+            payment_method_types: ["card"],
+            billing_address_collection: "required",
+            line_items: [
+                {
+                    price: process.env.STRIPE_PRICE,
+                    quantity: 1,
+                },
+            ],
+            mode: "subscription",
+            allow_promotion_codes: true,
+            success_url: process.env.STRIPE_SUCCESS_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL,
+        });
+
+        return {
+            sessionId: stripeCheckoutSession.id,
+        };
+    
     }
 }
 
